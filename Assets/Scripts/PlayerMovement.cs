c1e23b27045ecb0409d94c596af5867f4eadae41
JamesBabz <web.fuhlendorff@gmail.com>
2018-03-21 12:49:12 +0100

    init
    init commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed = 6.0F;
+    public float jumpHeight = 8.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    private bool isGrounded = false;
+    private float distToGround;
+
+    void Start()
+    {
+           distToGround = this.gameObject.GetComponent<BoxCollider>().bounds.extents.y;
+    }
+
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump") && isGrounded)
+                moveDirection.y = jumpHeight;
+            
+        this.transform.position += moveDirection;
+    }
+}
